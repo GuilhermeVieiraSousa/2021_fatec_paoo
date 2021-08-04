871d50287bca60b4b5ffa600309456fea6a31885
Guilherme <guilherme.v.sousa@hotmail.com>
2021-08-04 13:47:12 -0300

    Comit inicial. Classe empregado

---
+++ Empregado.java
@@ -0,0 +1,14 @@
+    public class Empregado{
+        private String nome;
+        private int idade;
+
+        public String getNome(){
+            return this,nome;
+        }
+
+        public int getIdafe(){
+            return this.idade;
+        }
+
+        public void 
+    }
