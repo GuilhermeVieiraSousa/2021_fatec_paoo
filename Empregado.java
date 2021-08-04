fbe6e3a211671e6b7bf4d898a56936a90264745e
Guilherme <guilherme.v.sousa@hotmail.com>
2021-08-04 14:04:29 -0300

    Calculo de salario efetuado

--- Empregado.java
+++ Empregado.java
@@ -1,6 +1,22 @@
     public class Empregado{
         private String nome;
         private int idade;
+        private double salario, comissao, bonus;
+
+        public double calcularSlario(){
+            if (tipo == 1){
+                return salario;
+            }
+            else if (tipo === 2){
+                return salario + salario * comissao;
+            }
+            else if (tipo == 3){
+                return salario + bonus;
+            }
+
+            return 0;
+        }
+
 
         public String getNome(){
             return this,nome;
@@ -10,5 +26,11 @@
             return this.idade;
         }
 
-        public void 
+        public void setName (String nome){
+            this.nome = nome;
+        }
+
+        public void setIdade (int idade){
+            this.idade = idade;
+        }
     }
